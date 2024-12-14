80f95a9d8fb042cd299df72d852919ba4efe97a7
Your Name <you@example.com>
2024-12-14 12:13:43 -0500

    fixup

--- accel/tcg/cputlb.c
+++ accel/tcg/cputlb.c
@@ -1233,7 +1233,7 @@ static bool tlb_fill_align(CPUState *cpu, vaddr addr, MMUAccessType type,
     const TCGCPUOps *ops = cpu->cc->tcg_ops;
     CPUTLBEntryFull full;
 
-	qemu_log("tlb_fill_align: addr=0x%lx, type=%d, mmu_idx=%d, memop=%d, " "size=%d, probe=%d\n", addr, type, mmu_idx, memop, size, probe);
+	qemu_log("tlb_fill_align: addr=0x%lx, type=%d, mmu_idx=%d, memop=%d, size=%d, probe=%d\n", addr, type, mmu_idx, memop, size, probe);
 
     if (ops->tlb_fill_align) {
 		qemu_log("tlb_fill_align: Using custom method\n");
@@ -1668,8 +1668,7 @@ static bool mmu_lookup1(CPUState *cpu, MMULookupPageData *data, MemOp memop,
     }
 
     full = &cpu->neg.tlb.d[mmu_idx].fulltlb[index];
-	//qemu_log("mmu_lookup1: full=%p, phys_addr=0x%lx, slow_flags[%d]=0x%x\n",
-			(void*)full, full->phys_addr, access_type, full->slow_flags[access_type]);
+	//qemu_log("mmu_lookup1: full=%p, phys_addr=0x%lx, slow_flags[%d]=0x%x\n", (void*)full, full->phys_addr, access_type, full->slow_flags[access_type]);
     flags = tlb_addr & (TLB_FLAGS_MASK & ~TLB_FORCE_SLOW);
     flags |= full->slow_flags[access_type];
 

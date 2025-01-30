4fb0cd93573ca779ad8a31c997c8f59bcdcd2702
R E Broadley <rebroad@users.noreply.github.com>
2025-01-30 06:10:14 -0500

    erm_sleep

--- target/sparc/cpu.c
+++ target/sparc/cpu.c
@@ -126,7 +126,7 @@ static bool sparc_cpu_exec_interrupt(CPUState *cs, int interrupt_request)
     static struct timespec last_true_time, last_false_time;
     static long long true_interval_ns, min_true_interval_ns, max_true_interval_ns;
     static long long false_interval_ns, min_false_interval_ns, max_false_interval_ns;
-    static long long to_sleep = 50000; // microseconds
+    static long to_sleep = 50000, erm_sleep = 50000; // microseconds
     static int current_true_streak = 0, current_false_streak = 0;
     static int max_true_streak = 0, min_true_streak = 0;
     static int max_false_streak = 0, min_false_streak = 0;
@@ -171,12 +171,13 @@ static bool sparc_cpu_exec_interrupt(CPUState *cs, int interrupt_request)
             max_false_interval_ns = (false_interval > max_false_interval_ns) ? false_interval : max_false_interval_ns;
 
             if (false_interval < 520) {
-				if (max_false_streak > 550 && min_true_streak == 1) to_sleep = 100000;
+				erm_sleep = to_sleep;
+				if (max_false_streak > 550 && min_true_streak == 1) erm_sleep = 100000;
                 sleeps++;
-                struct timespec sleep_duration = {0, to_sleep * 1000};
+                struct timespec sleep_duration = {0, erm_sleep * 1000};
                 timespecadd(&last_false_time, &sleep_duration);
                 timespecadd(&last_true_time, &sleep_duration);
-                usleep(to_sleep);
+                usleep(erm_sleep);
             }
         }
         memcpy(&last_false_time, &current_time, sizeof(struct timespec));
@@ -186,12 +187,12 @@ static bool sparc_cpu_exec_interrupt(CPUState *cs, int interrupt_request)
     time_t current_wall_time = time(NULL);
     if (current_wall_time != last_print_time) {
         printf("Interrupt Stats:\n"
-               "  Counts - True: %u, False: %u, to_sleep: %llu, sleeps: %u\n"
+               "  Counts - True: %u, False: %u, erm_sleep: %lu, sleeps: %u\n"
                "  Avg True Interval: %llu ns (Min/Max: %llu/%llu)\n"
                "  Avg False Interval: %llu ns (Min/Max: %llu/%llu)\n"
                "  True Streaks (this second): Max: %d, Min: %d\n"
                "  False Streaks (this second): Max: %d, Min: %d\n",
-               true_count, false_count, to_sleep, sleeps,
+               true_count, false_count, erm_sleep, sleeps,
                true_count ? true_interval_ns / true_count : 0,
                min_true_interval_ns, max_true_interval_ns,
                false_count ? false_interval_ns / false_count : 0,

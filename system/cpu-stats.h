abf3bf6091a5aac09307d1ca8f6f0a9cd7dacf89
R E Broadley <rebroad@users.noreply.github.com>
2025-04-26 22:48:23 +0100

    cpu-stats AI WIP

--- system/cpu-stats.h
+++ system/cpu-stats.h
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <glib.h> // For g_new0
+#include <stdbool.h>
+#include <limits.h> // For UINT64_MAX, INT_MAX
 
 #ifndef CPU_STATS_H
 #define CPU_STATS_H
@@ -10,21 +12,34 @@
 #define MAX_DEBUG_FUNCS 128  /* Maximum number of functions we'll track */
 #define NUM_STATES 3 /* PROM_IDLE, OS_IDLE, SHUTDOWN */
 #define STATE_EDGE_FILE "state_edges.dat"
+#define MAX_FUNC_NAME_LEN 64 // Max length for function names
+#define STATE_EDGE_MAGIC 0xCPU57A75 // Magic number for file format
+#define STATE_EDGE_VERSION 1        // File format version
 
 /* Structure to hold the min/max edges for counters per function */
+/* This structure is used both in the save file and runtime */
 struct func_state_edges {
-    uint64_t min_ns[2][2]; // Min/Max observed value for min_ns[0/1]
-    uint64_t max_ns[2][2]; // Min/Max observed value for max_ns[0/1]
-    uint64_t avg_ns[2][2]; // Min/Max observed value for avg_ns[0/1]
-    int min_streak[2][2]; // Min/Max observed value for min_streak[0/1]
-    int max_streak[2][2]; // Min/Max observed value for max_streak[0/1]
-    int true_count[2][2]; // Min/Max observed value for true_count[0/1]
-    int false_count[2][2]; // Min/Max observed value for false_count[0/1]
+    char func_name[MAX_FUNC_NAME_LEN]; // Function name associated with these edges
+    // Format: stat[true/false][min/max]
+    uint64_t min_ns[2][2];
+    uint64_t max_ns[2][2];
+    uint64_t avg_ns[2][2];
+    int min_streak[2][2];
+    int max_streak[2][2];
+    int count[2][2]; // Min/Max observed value for count[0/1]
 };
 
 /* Structure to hold edges for all functions within a specific state */
 struct state_edges {
+    // Note: func_name is now part of func_state_edges
     struct func_state_edges func_edges[MAX_DEBUG_FUNCS];
+};
+
+/* Header structure for the state edge file */
+struct state_edge_file_header {
+    uint32_t magic;
+    uint32_t version;
+    uint32_t num_funcs_saved;
 };
 
 /* Global variables for debug counters */
@@ -36,18 +51,17 @@ struct state_edges state_edge_data[NUM_STATES];
 bool state_edges_loaded = false;
 
 void cpu_stats_per_second(void);
+struct debug_counters *find_counters_array(const char *func_name);
 
 /* Debug function macro to collect statistics */
 #define DEBUG_FUNC() \
     static struct debug_counters *counters = NULL; \
     do { \
         if (!counters) { \
-            int func_id = next_func_id++; \
-            if (func_id <= MAX_DEBUG_FUNCS) { \
+            if (next_func_id++ <= MAX_DEBUG_FUNCS) { \
                 if (!counters_array) \
                     counters_array = g_new0(struct debug_counters, MAX_DEBUG_FUNCS); \
-                counters = &counters_array[func_id - 1]; \
-                counters->func_name = __func__; \
+                counters = find_counters_array(__func__); \
             } \
         } \
         if (counters) counters->call_count++; \

4747ea6e56bc342015f282426256bed83b5b5249
R E Broadley <rebroad@users.noreply.github.com>
2025-10-13 12:40:26 +0200

    fixup! remove leftovers

--- system/runstate.c
+++ system/runstate.c
@@ -885,9 +885,6 @@ void qemu_init_subsystems(void)
 
     bdrv_init_with_whitelist();
     socket_init();
-
-    // Initialize CPU stats
-    // cpu_stats_init();
 }
 
 
--- target/i386/kvm/kvm.c
+++ target/i386/kvm/kvm.c
@@ -5630,7 +5630,6 @@ int kvm_arch_process_async_events(CPUState *cs)
 
 static int kvm_handle_halt(X86CPU *cpu)
 {
-	// REBTODO - similar to sparc_cpu_exec_interrupt or sparc_cpu_has_work
     CPUState *cs = CPU(cpu);
     CPUX86State *env = &cpu->env;
 
